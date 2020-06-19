39d7af67e6b7c7dd46fb5a297f9d505a432959ef
veucosta <67131899+veucosta@users.noreply.github.com>
2020-06-19 19:06:37 -0400

    Initial upload of the Ômega site project
    Upload files (images, scripts, styles) to the repository.

---
+++ omega/js/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+$('.slider').slick({
+    autoplay: true, // Habilitando a auto-reprodução
+    autoplaySpeed: 4000,
+    dots: true // Habilitando os pontos abaixo do slide
+    // arrows: false -> serve para desabilitar as setas ao lado das imagens
+});
+
+$('#telefone').mask('(00) 0000-0000');
+
+// Script do Formspree
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+        form.reset();
+        button.style = "display: none ";
+        status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+        status.innerHTML = "Oops! There was a problem.";
+        }
+
+        // handle the form submission event
+
+        form.addEventListener("submit", function(ev) {
+            ev.preventDefault();
+            var data = new FormData(form);
+            ajax(form.method, form.action, data, success, error);
+        });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+    } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+    }
+    };
+    xhr.send(data);
+}
